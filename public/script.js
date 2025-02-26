812087a160f4a13e928fcfb7141cefda7588a17c
Meera-Persad <meera.persad2004@gmail.com>
2025-02-26 15:37:54 -0400

    Meera-Persad Lab6-part1

---
+++ public/script.js
@@ -0,0 +1,19 @@
+
+let myobj = {name: 'Bob'}; 
+
+let obj2 = myobj;
+
+//obj2 and myobj are the same variable
+
+obj2.name = "Shelly";
+
+console.log("myobj", myobj.name);//Shelly
+
+//if you need to create a new object and copy its values use Object.assign
+
+let obj3 = {};
+Object.assign(obj3, myobj);
+
+obj3.name = 'Smith';
+
+console.log(myobj.name, obj3.name);//Shelly, Smith
